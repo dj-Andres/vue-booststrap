a7858364677cbfb0628148835f052601310d599a
dj-Andres <andres96jimenez@gmail.com>
2020-12-05 17:16:33 -0500

    creando componentes y rutas con bootstrap

---
+++ src/views/Cards.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mt-5">
+        <b-row class="mt-5">
+            <b-col cols="12" md="4">
+                <b-card
+                    img-src="https://placekitten.com/380/200"
+                    img-top
+                >
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit repudiandae ut ipsam, vero accusantium.
+                </p>
+                <b-button variant="dark" href="#">Agregar</b-button>
+                </b-card>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+<script>
+export default {
+    name:'Cards'
+}
+</script>
---
+++ src/views/Grid.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="mt-5">
+      <b-row>
+          <b-col cols="12" md="6">
+              <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, voluptatem temporibus. Fuga.
+              </p>
+          </b-col>
+          <b-col cols="12" md="6">
+              <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, voluptatem temporibus. Fuga.
+              </p>
+          </b-col>
+      </b-row>
+    </div>
+</template>
+<script>
+export default {
+    name:'Grid'
+}
+</script>
